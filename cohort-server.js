0accb985b7442652bc38bb59adb84d8d6f7c812a
Jacob Niedzwiecki <jake@jacob-n.com>
2018-08-16 13:21:14 -0400

    Added elapsed time tracking for broadcast

--- cohort-server.js
+++ cohort-server.js
@@ -9,6 +9,7 @@ const wsServer = new WebSocket.Server({ port: 8080, clientTracking: true }, () =
 });
 
 var expectedConfirmations, actualConfirmations;
+var elapsedTimeForBroadcast;
 
 // configure express
 var jsonParser = bodyParser.json();
@@ -20,6 +21,7 @@ app.get('/', function(request, response){
 });
 
 app.post('/broadcast', jsonParser, function(request, response){
+	elapsedTimeForBroadcast = Date.now();
 	const waitForConfirmations = request.body.waitForConfirmations * 1000; // TODO This can be null / undefined, fix it!!!
 	wsServer.broadcast(JSON.stringify(request.body.payload));
 	response.writeHead(200);
@@ -31,39 +33,43 @@ wsServer.broadcast = function broadcast(message) {
 	expectedConfirmations = wsServer.clients.size;
 	actualConfirmations = 0;
 
+	// this might be faster?
 	// per https://github.com/websockets/ws/issues/617#issuecomment-393396339
-	let data = Buffer.from(message); // only text messages are supported, no binary
-	let frames = WebSocket.Sender.frame(data, {
-		fin: true,
-		rsv1: false,
-		opcode: 1,
-		mask: false,
-		readOnly: false
-	});
-
-	return [...wsServer.clients].map( (socket) => {
-		// skip this socket if it isn't open
-		if(socket.readyState !== WebSocket.OPEN) {
-			return promise.resolve();
-		}
-
-		return new Promise( (fulfill, reject) => {
-			socket._sender.sendFrame(frames, (error) => {
-				if(error){ 
-					// catch async socket write errors
-					console.log(error);
-					return reject(error);
-				}
-				fulfill();
-			});
-		});
-	});
-
-	// wsServer.clients.forEach(function each(client) {
-	// 	if (client.readyState === WebSocket.OPEN) {
-	// 		client.send(data);
-	// 	}
+	// let data = Buffer.from(message); // only text messages are supported, no binary
+	// let frames = WebSocket.Sender.frame(data, {
+	// 	fin: true,
+	// 	rsv1: false,
+	// 	opcode: 1,
+	// 	mask: false,
+	// 	readOnly: false
 	// });
+
+	// return [...wsServer.clients].map( (socket) => {
+	// 	// skip this socket if it isn't open
+	// 	if(socket.readyState !== WebSocket.OPEN) {
+	// 		return promise.resolve();
+	// 	}
+
+	// 	return new Promise( (fulfill, reject) => {
+	// 		socket._sender.sendFrame(frames, (error) => {
+	// 			if(error){ 
+	// 				// catch async socket write errors
+	// 				console.log(error);
+	// 				return reject(error);
+	// 			}
+	// 			fulfill();
+	// 		});
+	// 	});
+	// });
+
+	wsServer.clients.forEach(function each(client) {
+		if (client.readyState === WebSocket.OPEN) {
+			client.send(data);
+		}
+	});
+
+	const elapsedTime = Date.now() - elapsedTimeForBroadcast;
+	elapsedTimeForBroadcast = elapsedTime;
 };
 
 app.listen(3000, function(err){
@@ -99,7 +105,7 @@ function checkConfirmations(response){
 	if(expectedConfirmations != actualConfirmations){
 		console.log("WARNING: one or more clients may not have received broadcast");
 	}
-
+	response.write("\n    ...broadcast took " + elapsedTimeForBroadcast/1000 + "s");
 	response.write("\n    ..." + actualConfirmations + "/" + expectedConfirmations + " clients confirmed receipt");
 	response.send();
 }
