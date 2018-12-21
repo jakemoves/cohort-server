edf8c64ed0c312617501b9e57cf364b6f7360fe4
Jacob Niedzwiecki <jacob@jakemoves.com>
2018-12-20 21:01:19 -0500

    Added close and error events for sockets

--- cohort-websockets.js
+++ cohort-websockets.js
@@ -12,45 +12,55 @@ module.exports = (options) => {
 
   webSocketServer.on('connection', function connection(socket) {
     console.log('websocket server: new connection')
-    socket.isAlive = true
+
     let device = new CHDevice()
+
+    socket.isAlive = true
+    socket.on('pong', keepalive)
+
+    socket.on('close', (code, reason) => {
+      let message = 'closed socket for device ' + device.guid + ' with code ' + code
+      if(reason != null){ message += ' | reason: ' + reason }
+      console.log(message)
+      device.socket = null
+    })
+
+    socket.on('error', (error) => {
+      console.log('socket error for device ' + device.guid + ': ' + error)
+    })
+    
     device.socket = socket
     let devices = options.app.get('cohort').devices
     devices.push(device)
     console.log('created new device: ' + devices.length)
   })
 
-  // TODO Verify this actually works!!
-
-  function keepalive() {
-    console.log('received pong')
-    this.isAlive = true
-  }
-
-  function noop() {}
-
   const interval = setInterval(function ping(){
-    const devicesWithSockets = options.app.get('cohort').devices.filter((device) => {
-      return device.socket != null && typeof device.socket != undefined
-    })
-    console.log("sending keepalive to " + devicesWithSockets.length + " clients");
-    devicesWithSockets.forEach( (device) => {
-      if(device.socket.isAlive === false){
-        device.socket.terminate()
+
+    const connectedSockets = options.app.get('cohort').devices
+      .filter((device) => {
+        return device.socket != null && typeof device.socket != undefined
+      })
+
+    // console.log("sending keepalive to " + connectedSockets.length + " clients");
+
+    connectedSockets.forEach( (device) => {
+      let socket = device.socket
+      if(socket.isAlive === false){
+        socket.terminate()
         device.socket = null
       } else {
-        device.socket.isAlive = false
-        device.socket.ping(noop)
+        socket.isAlive = false
+        socket.ping(noop)
       }
     })
   }, 10000)
 
-  // NEVER HAPPENS
-  // ||  ||  ||
-  // \/  \/  \/
-  webSocketServer.on('pong', (socket) => {
-    console.log('received pong')
-    socket.isAlive = true
-  })
+  function noop() {}
+  
+  function keepalive() {
+    // console.log('received pong')
+    this.isAlive = true
+  }
 
 }
