76c0f2e263b8114abe4af41836950a3c62428a38
Jacob Niedzwiecki <jacob@jakemoves.com>
2019-01-04 22:13:18 -0500

    Refactored using fetch

--- public/js/cohort-admin-client.js
+++ public/js/cohort-admin-client.js
@@ -1,66 +1,79 @@
 var vm = new Vue({
   el: '#cohort-admin',
   data: {
-    events: [],
-    event: '1',
-    devices: []
+    events: []
   }
 })
 
 let guid = 12345
-let req = new XMLHttpRequest()
-req.open('POST', 'http://localhost:3000/api/devices')
-req.setRequestHeader('Content-Type', 'application/json')
 
-req.onload = () => {
-  switch(req.status){
-    case 200: 
-      let checkInReq = new XMLHttpRequest()
-      checkInReq.open('PATCH', 'http://localhost:3000/api/events/1/check-in')
-      checkInReq.setRequestHeader('Content-Type', 'application/json')
-      checkInReq.onload = () => {
-        // this admin device is checked in for event 1
-        const client = new WebSocket('ws://localhost:3000')
-
-        client.addEventListener('open', () => {
-          console.log('connection open')
-          client.send(JSON.stringify({ guid: 12345 }))
-        })
-
-        client.addEventListener('message', (message) => {
-          const msg = JSON.parse(message.data)
-          console.log(msg)
-          if(msg.status != null && msg.status != undefined){
-            vm.devices = msg.status
-            // let deviceCount = document.getElementsByClassName('connected-devices').item(0)
-            // deviceCount.innerHTML = msg.status.length
-
-            // let deviceList = ""
-            // let deviceListItems = msg.status.forEach( device => {
-            //   let styleClass = ""
-            //   if(device.webSocketState != WebSocket.OPEN){
-            //     styleClass = ' class="not-connected"'
-            //   }
-            //   deviceList += "<li" + styleClass + ">#" + device.guid + "</li>"
-            // })
-            // document.getElementsByClassName('device-list').item(0).innerHTML = deviceList
-          }
-        })
-
-        client.addEventListener('close', () => {
-          console.log('connection closed')
-        })
-
-        client.addEventListener('error', (err) => {
-          console.log(err)
-        })
-      }
-      checkInReq.send(JSON.stringify({ guid: guid }))
-      break;
-
-    default:
-      break;
+fetch('http://localhost:3000/api/events', {
+  method: 'GET'
+}).then( response => {
+  if(response.status == 200){
+    response.json().then( events => {
+      vm.events = events
+    })
+  } else {
+    response.text().then( errorText => {
+      console.log(errorText)
+    })
   }
-}
+})
 
-req.send(JSON.stringify({ guid: guid, isAdmin: true }))
+// fetch('http://localhost:3000/api/devices', {
+//   method: 'POST',
+//   headers: { 'Content-Type': 'application/json'},
+//   body: JSON.stringify({ guid: guid })
+// }).then( res => {
+//   if(res.status == 200){
+    
+//   } else {
+//     throw new Error(res.status + ": " + res.text)
+//   }
+// })
+
+// let req = new XMLHttpRequest()
+// req.open('POST', 'http://localhost:3000/api/devices')
+// req.setRequestHeader('Content-Type', 'application/json')
+
+// req.onload = () => {
+//   switch(req.status){
+//     case 200: 
+//       let checkInReq = new XMLHttpRequest()
+//       checkInReq.open('PATCH', 'http://localhost:3000/api/events/1/check-in')
+//       checkInReq.setRequestHeader('Content-Type', 'application/json')
+//       checkInReq.onload = () => {
+//         // this admin device is checked in for event 1
+//         // const client = new WebSocket('ws://localhost:3000')
+
+//         // client.addEventListener('open', () => {
+//         //   console.log('connection open')
+//         //   client.send(JSON.stringify({ guid: 12345 }))
+//         // })
+
+//         // client.addEventListener('message', (message) => {
+//         //   const msg = JSON.parse(message.data)
+//         //   console.log(msg)
+//         //   if(msg.status != null && msg.status != undefined){
+//         //     // vm.devices = msg.status
+//         //   }
+//         // })
+
+//         // client.addEventListener('close', () => {
+//         //   console.log('connection closed')
+//         // })
+
+//         // client.addEventListener('error', (err) => {
+//         //   console.log(err)
+//         // })
+//       }
+//       checkInReq.send(JSON.stringify({ guid: guid }))
+//       break;
+
+//     default:
+//       break;
+//   }
+// }
+
+// req.send(JSON.stringify({ guid: guid, isAdmin: true }))
