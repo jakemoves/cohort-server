412cd5f3c8c86ffba8289f897975017d0d3b9b5a
Jacob Niedzwiecki <jacob@jakemoves.com>
2019-01-09 03:37:17 +0000

    Added support for CORS, especially annoying with safari

--- src/cohort-app.js
+++ src/cohort-app.js
@@ -9,6 +9,14 @@ const app = express()
 const jsonParser = bodyParser.json()
 const routes = require('./routes.js')
 app.use(bodyParser.json())
+
+app.use( (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+  next()
+})
+
 app.use('/api', routes)
 app.use(express.static('public'))
 
