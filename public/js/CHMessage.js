b158d08b13513b52fdd3746557fc20deb7d54dbc
Jacob Niedzwiecki <jacob@jakemoves.com>
2019-03-18 14:25:25 -0400

    Added fake enum for cue action

--- public/js/CHMessage.js
+++ public/js/CHMessage.js
@@ -6,9 +6,17 @@ module.exports = function CHMessage(domain, cueNum, cueAct) {
   }
   mediaDomains.freeze
 
+  let cueActions = {
+    "play": 0,
+    "pause": 1,
+    "restart": 2,
+    "stop": 3
+  }
+
   let msg = {}
   msg.mediaDomain = mediaDomains[domain]
   msg.cueNumber = cueNum
-  msg.cueAction = cueAct
+  msg.cueAction = cueActions[cueAct]
+  
   return msg
 }
