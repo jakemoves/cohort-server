ebd4170da36b283a1d0c74cbfea334f9e02e4ec1
Jacob Niedzwiecki <jacob@jakemoves.com>
2019-03-21 16:47:03 -0400

    Fix for authkey issue

--- src/cohort-apple-notifications.js
+++ src/cohort-apple-notifications.js
@@ -1,10 +1,22 @@
 const apn = require('apn')
 
-var options = {
-	token: {
-		key: "./AuthKey_6TA7832PAJ.p8",
-		keyId: "6TA7832PAJ",
-		teamId: "J93D25NHHG"
+var options 
+
+if(process.env.NODE_ENV == 'production'){
+	options = {
+		token: {
+			key: "../AuthKey_6TA7832PAJ.p8",
+			keyId: "6TA7832PAJ",
+			teamId: "J93D25NHHG"
+		}
+	}
+} else if(process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'test'){
+	options = {
+		token: {
+			key: "./AuthKey_6TA7832PAJ.p8",
+			keyId: "6TA7832PAJ",
+			teamId: "J93D25NHHG"
+		}
 	}
 }
 
