ee5308ce6a1518c3eb0c4d0a2583574a30ee7a97
Jacob Niedzwiecki <jacob@jakemoves.com>
2019-03-27 12:20:22 -0400

    WIP on occasion notifications, fresh bugs

--- src/knex/migrations/20190321094235_add_occasions_to_events_devices_table.js
+++ src/knex/migrations/20190321094235_add_occasions_to_events_devices_table.js
@@ -11,8 +11,8 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return knex.schema.alterTable('events_devices', table => {
-    // table.dropUnique(['event_id', 'device_id', 'occasion_id'])
+    table.dropUnique(['event_id', 'device_id', 'occasion_id'])
     table.dropColumn('occasion_id')
-    // table.unique(['event_id', 'device_id'])
+    table.unique(['event_id', 'device_id'])
   })
 }
