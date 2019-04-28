68b0277e6ec4877aca3319dd67174fb4dfb864cc
Jacob Niedzwiecki <jacob@jakemoves.com>
2019-04-28 14:27:02 -0400

    Minor tweak to APNS target environment

--- src/cohort-apple-notifications.js
+++ src/cohort-apple-notifications.js
@@ -3,7 +3,12 @@ const apn = require('apn')
 function start(){
 	var options 
 
-	console.log("preparing notification service for '" + process.env.NODE_ENV + "' environment")
+	const useSandboxAPNS = false // set to TRUE if you are testing local builds from Xcode against this server, or you will get 400 errors (Bad Device Token) b/c those builds always use the APNS sandbox
+
+	let apnsEnv = process.env.NODE_ENV
+	if(useSandboxAPNS){ apnsEnv = "sandbox" }
+
+	console.log("preparing notification service using '" + apnsEnv + "' environment")
 	
 	if(process.env.NODE_ENV == 'production'){
 		options = {
@@ -12,7 +17,7 @@ function start(){
 				keyId: "6TA7832PAJ",
 				teamId: "J93D25NHHG"
 			}
-			//, production: false // uncomment if you are testing local builds from Xcode against this server, or you will get 400 errors (Bad Device Token) b/c those builds always use the APNS sandbox
+			, production: useSandboxAPNS 
 		}
 	} else if(process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'test'){
 		options = {
