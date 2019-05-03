b9f21a7b0fffcd4e414bdad09b884fdaf9517c6f
Jacob Niedzwiecki <jacob@jakemoves.com>
2019-05-03 10:22:07 -0400

    Tidied up logging

--- src/cohort-websockets.js
+++ src/cohort-websockets.js
@@ -7,7 +7,7 @@ module.exports = (options) => {
     let webSocketServer = new webSocket.Server({server: options.server, path: options.path, clientTracking: true})
     
     const keepaliveInterval = setInterval(function ping(){
-      console.log('keepaliveInterval(), ' + webSocketServer.clients.size + ' clients attached')
+      // console.log('keepaliveInterval(), ' + webSocketServer.clients.size + ' clients attached')
 
       webSocketServer.clients.forEach( (socket) => {
         if(socket.isAlive === false){
