d8c50803f6a4887cf91cf7355901c40f9b248498
Luke Garwood <luke_gar@hotmail.com>
2019-10-24 00:59:46 -0400

    created 'focused' events and occasions

--- admin-site/src/App.svelte
+++ admin-site/src/App.svelte
@@ -1,16 +1,14 @@
 <script>
 	import Login from './Login.svelte';
-	import Event from './Events.svelte';
 	import Slider from './Slider.svelte';
 
-
-	const LotX =[{
+	let events =[{
 					"id": 1,
 					"label": "LOT X",
 				//"heroImage": URL-TO-IMG, // optional
 					"occasions": [
 						{
-						"id": 3,
+						"id": 1,
 						"event_id": 1,
 						"state": "closed", // can be open or closed; closed events cannot be joined
 						"startDateTime": "2019-05-23T17:00:00.000Z", // stored in UTC, browser does conversion
@@ -19,16 +17,16 @@
 						"locationLabel": "Show #5",
 						"locationAddress": "125 Emerson Ave, Toronto ON, M6H 3S7",
 						"locationCity": "Toronto",
-					"publicUR": "https://cohort.rocks/api/v2/events/1/occasions/3", // for making QR code to join the event
-					"devices": [
-						{
-						"id": 1,
-						"guid": "dklfjdklf-dfd-f-df-dfdfdfas-3r3r-fdf3",
-						"apnsDeviceToken": null, // not used for now -- this is for push notifications
-						"isAdmin": true, // here for now -- the admin site will connect to an occasion as a device
-						"tags": ["blue", "1984"]
-						}
-					]
+						"publicURL": "https://cohort.rocks/api/v2/events/1/occasions/3", // for making QR code to join the event
+						"devices": [
+							{
+							"id": 1,
+							"guid": "dklfjdklf-dfd-f-df-dfdfdfas-3r3r-fdf3",
+							"apnsDeviceToken": null, // not used for now -- this is for push notifications
+							"isAdmin": true, // here for now -- the admin site will connect to an occasion as a device
+							"tags": ["blue", "1984"]
+							}
+						]
 					}
 				],
 				"cues": [
@@ -41,35 +39,52 @@
 				]
 			}];
 
-	let events =[LotX];
-
+//for new event creation parameters
 	let label = '';
-	let newShow = [];
-	let OccasionListState;
+
+//Holds event ID in order to display occasions for that event
+	let focusedEventID="0";
+	let focusedOccasionID="0";
+	let focusedEvent="0";
+	let focusedOccasion="0";
+//only works if id numbers are set in order (currently starting from 1)
+	let indexInEvents;
+	let indexInOccasions;
 
 // when an event button is hit only open occasions for that event
 	function eventButton(){
 		document.getElementById("eventsList").style.display = "none";
 		document.getElementById("occasionList").style.display = "block";
-		OccasionListState = this.value;
+		focusedEventID = this.value;
+		indexInEvents = focusedEventID - 1;
+		focusedEvent = events[indexInEvents];
+		
 
 	}
+
+	function occasionButton(){
+		focusedOccasionID= this.value;
+		document.getElementById("occasionList").style.display = "none";
+		document.getElementById("closeEvent").style.display = "block";
+		
+		indexInOccasions = focusedOccasionID - 1;
+		focusedOccasion = focusedEvent.occasions[indexInOccasions];
+		
+	}
+
+/////for new event generation
 	function setTitle(event){
 		label = event.target.value;
 	}
 
 	function createEvent(){
-		let newEvent = [[{
+		let newEvent = [{
 				label:label
-			}]];
-			
-		
-		// const newEvent = [{
-		// 	label:label
-		// }];
+			}];
 		events = events.concat(newEvent);
 		console.log(events);
 	}
+////////
 </script>
 
 <style>
@@ -77,6 +92,15 @@
 		display: none;
 	}
 	#occasionList{
+		display: none;
+	}
+	#closeEvent{
+		display: none;
+	}
+	#openEvent{
+		display: none;
+	}
+	#QRcode{
 		display: none;
 	}
 	
@@ -97,22 +121,15 @@
 		{#if events.length === 0}
 			<p>No events have been added yet</p>
 			{:else}
-			{#each events as show}
-			{#if show.length > 0}
-				{#each show as event}
-				
-					<button type="button" class= 'btn btn-primary btn-block' value = {event.id} on:click={eventButton} >
-						<h3>{event.label}</h3>	
-					</button>
-				
-				{/each}
-			{/if}
-			{/each}
+					{#each events as event}
+					
+						<button type="button" class= 'btn btn-primary btn-block' value = {event.id} on:click={eventButton} >
+							<h3>{event.label}</h3>	
+						</button>
+					
+					{/each}
 		{/if}
-	<!-- </section> -->
-
 <!-- event creation -->
-	<!-- <section> -->
 	<hr>
 		<div>
 			<label for="title">New Event Name</label> 
@@ -129,20 +146,15 @@
 		{#if events.length === 0}
 			<p>No events have been added yet</p>
 			{:else}
-			{#each events as show}
-				{#if show.length > 0}
-					{#each show as event}
-							{#if event.id == OccasionListState && event.occasions != null}
+					{#each events as event}
+							{#if event.id == focusedEventID && event.occasions != null}
 								{#each event.occasions as occasion}	
-									<button type="button" class= 'btn btn-primary btn-block'>
+									<button type="button" class= 'btn btn-primary btn-block' value = {occasion.id} on:click={occasionButton}>
 										<h3>{event.label} - {occasion.startDateTime}</h3>	
 									</button>
 								{/each}
 							{/if}
 					{/each}
-				{/if}
-			{/each}
-			
 		{/if}
 	</section>
 </div>
@@ -154,7 +166,7 @@
   		<div class="modal-dialog modal-lg">
     		<div class="modal-content">
 				<div class="modal-header">
-        			<h5 class="modal-title">{events[0].label} - </h5>
+        			<h5 class="modal-title"> blank</h5>
         				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
           					<span aria-hidden="true">&times;</span>
         				</button>
@@ -230,31 +242,31 @@
 		<div class="modal-dialog modal-lg">
     		<div class="modal-content">
 				<div class="modal-header">
-        			<h5 class="modal-title">{events[0].label}</h5>
+        			<h5 class="modal-title">{focusedEvent.label} - {focusedOccasion.startDateTime}.</h5>
       			</div>
 
 				<div class="modal-body">
 					<div class="container-fluid">
 						<div class="row">
 							<div class="col-md-12">
-								<label for="OccasionDetails"><h4>Event Details</h4></label>
+								<label for="OccasionDetails"><h4>Occasion Details</h4></label>
 								<ul id="OccasionDetails">
-									<!-- <li>Start Date : {events[0].occasions[0].startDateTime}</li>
-									<li>End Date : {events[0].occasions.endDateTime}</li>
-									<li>Location Label: {events[0].occasions.locationLabel} </li>
-									<li>Location Address: {events[0].occasions.locationAddress} </li>
-									<li>Location City: {events[0].occasions.locationCity} </li> -->
+									<li>Start Date : {focusedOccasion.startDateTime}</li>
+									<li>End Date : {focusedOccasion.endDateTime}</li>
+									<li>Location Label: {focusedOccasion.locationLabel} </li>
+									<li>Location Address: {focusedOccasion.locationAddress} </li>
+									<li>Location City: {focusedOccasion.locationCity} </li>
 								</ul> 	
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-md-12">
-								<button type="button" class="btn btn-danger btn-block">Delete Event</button>
+								<button type="button" class="btn btn-danger btn-block">Delete Occasion</button>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-md-12">
-								<button type="button" class="btn btn-success btn-block">Open Event</button>
+								<button type="button" class="btn btn-success btn-block">Open Occasion</button>
 							</div>
 						</div>
 						<div class="row">
