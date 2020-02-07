eea0046e6421f52d487d142ea2c83d7eef963fb5
Luke Garwood <luke_gar@hotmail.com>
2020-02-07 12:18:56 -0500

    changed default url

--- admin-site/src/ServerURLstore.js
+++ admin-site/src/ServerURLstore.js
@@ -1,6 +1,6 @@
 import {writable} from 'svelte/store';
 
-export let urlStore = writable("http://localhost:3000/api/v2");
+export let urlStore = writable("https://staging.cohort.rocks/api/v2");
 export let serverURL;
 
 urlStore.subscribe(value => {
