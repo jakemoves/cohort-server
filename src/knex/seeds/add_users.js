da95724ddcd1964a2ed358f9f409f95fda16be5f
Jacob Niedzwiecki <jake@jacob-n.com>
2020-02-23 13:04:27 -0500

    Added safety check to seed data

--- src/knex/seeds/add_users.js
+++ src/knex/seeds/add_users.js
@@ -1,25 +1,29 @@
 require('dotenv').config({ path: __dirname + '/../../../.env' })
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('users').del()
-  .then(function () {
-    return knex('users').insert([{
-      username: 'dev_user', // admin
-      password: process.env.DEV_USER_PASSWORD_HASH,
-      is_admin: true
-    },{
-      username: 'test_admin_user', // admin
-      password: process.env.TEST_ADMIN_USER_PASSWORD_HASH,
-      is_admin: true
-    },{
-      username: 'test_user_1',
-      password: process.env.TEST_USER_1_PASSWORD_HASH,
-      is_admin: false
-    },{
-      username: 'test_user_2',
-      password: process.env.TEST_USER_2_PASSWORD_HASH,
-      is_admin: false
-    }])
-  });
+  if(process.env.NODE_ENV != 'production'){
+    // Deletes ALL existing entries
+    return knex('users').del()
+    .then(function () {
+      return knex('users').insert([{
+        username: 'dev_user', // admin
+        password: process.env.DEV_USER_PASSWORD_HASH,
+        is_admin: true
+      },{
+        username: 'test_admin_user', // admin
+        password: process.env.TEST_ADMIN_USER_PASSWORD_HASH,
+        is_admin: true
+      },{
+        username: 'test_user_1',
+        password: process.env.TEST_USER_1_PASSWORD_HASH,
+        is_admin: false
+      },{
+        username: 'test_user_2',
+        password: process.env.TEST_USER_2_PASSWORD_HASH,
+        is_admin: false
+      }])
+    });
+  } else {
+    return
+  }
 }; 
