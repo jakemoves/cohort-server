cc8e24f9b79f97858b07a2892c314e692894c4df
Jacob Niedzwiecki <jake@jacob-n.com>
2020-10-21 12:12:11 -0600

    Added client-side ping/pong

--- src/cohort-websockets.js
+++ src/cohort-websockets.js
@@ -66,6 +66,20 @@ module.exports = (options) => {
           return
         }
 
+        // hacky endpoint for status updates for clients
+        if(msg.action == "client_ping"){
+          if(msg.clientGuid === undefined){
+            return
+          }
+          const payload = {
+            dataIdentifier: "client_pong",
+            clientGuid: msg.clientGuid
+          }
+          const jsonPayload = JSON.stringify(payload)
+          socket.send(jsonPayload)
+          return
+        }
+
         // handle initial handshake with device
         if(socket.cohortDeviceGUID === undefined || 
            socket.cohortDeviceGUID == null){
@@ -113,20 +127,6 @@ module.exports = (options) => {
           device.socket.send(JSON.stringify({ response: "success" }))
 
           // event.broadcastDeviceStates() // eventually this should get triggered by a deviceStatesDidChange event bubbled up from CHDevice... I think?
-        }
-
-        // hacky endpoint for status updates for clients
-        if(msg.action == "client_ping"){
-          if(msg.clientGuid === undefined){
-            return
-          }
-          const payload = {
-            dataIdentifier: "client_pong",
-            clientGuid: msg.clientGuid
-          }
-          const jsonPayload = JSON.stringify(payload)
-          socket.send(jsonPayload)
-          return
         }
       })
 
