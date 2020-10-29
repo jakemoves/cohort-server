3abae788a1fcb8a656360e0e416b7d4a740ae2b8
Luke Garwood <luke_gar@hotmail.com>
2020-10-29 15:46:04 -0400

    added links on get-started that are relative to current url

---
+++ public/assets/js/dynamicLinks.js
@@ -0,0 +1,11 @@
+//step 6 dynamic url
+
+const insertDynamicLink = () => {
+  const currentUrlHostname = window.location.hostname;
+  const eventLandingPageURL = `/join/occasions/6/web/true" target="_blank`;
+  const adminURL = '/admin';
+
+  document.getElementById('step6').innerHTML += `<p class = "text-left mt-4">Alternatively, you can <a href="${eventLandingPageURL}"> click this link </a> to open a recipient browser tab on your current device.</p>`;
+  document.getElementById('step1').innerHTML += `<p class="text-left">Open up a new tab with our admin panel at <a href="${adminURL}" target="_blank"> ${window.location.protocol}//${currentUrlHostname}/admin by clicking here</a></p>`
+}
+insertDynamicLink();
