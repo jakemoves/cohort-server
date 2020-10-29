0e568fc66a739aaf182dff642fe18e7562a4f6bd
Luke Garwood <luke_gar@hotmail.com>
2020-10-29 15:47:26 -0400

    changed wording on step 1 in get-started

--- public/assets/js/dynamicLinks.js
+++ public/assets/js/dynamicLinks.js
@@ -6,6 +6,6 @@ const insertDynamicLink = () => {
   const adminURL = '/admin';
 
   document.getElementById('step6').innerHTML += `<p class = "text-left mt-4">Alternatively, you can <a href="${eventLandingPageURL}"> click this link </a> to open a recipient browser tab on your current device.</p>`;
-  document.getElementById('step1').innerHTML += `<p class="text-left">Open up a new tab with our admin panel at <a href="${adminURL}" target="_blank"> ${window.location.protocol}//${currentUrlHostname}/admin by clicking here</a></p>`
+  document.getElementById('step1').innerHTML += `<p class="text-left">Open up a new tab for our admin panel at <a href="${adminURL}" target="_blank"> ${window.location.protocol}//${currentUrlHostname}/admin by clicking here</a></p>`
 }
 insertDynamicLink();
