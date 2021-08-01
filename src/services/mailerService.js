d7253cb67885a1d1d6d15d340567f64470296c33
Jacob Niedzwiecki <jacob@jakemoves.com>
2021-08-01 16:24:54 -0700

    Mailer service gracefully handles lack of internet connection

--- src/services/mailerService.js
+++ src/services/mailerService.js
@@ -3,7 +3,8 @@
 
 const nodemailer = require('nodemailer')
 
-var transporter = "test" 
+var transporter = "test"
+var connectedToMailServer = false;
 
 exports.initService = async function(){
   transporter = await nodemailer.createTransport({
@@ -18,18 +19,32 @@ exports.initService = async function(){
 
   transporter.verify( (error, success) => {
     if(error){
-      console.log(error)
+      // no internet connection?
+      if(error.errno == "ECONNREFUSED" &&
+        error.code == 'EDNS' &&
+        error.command == 'CONN' &&
+        error.hostname == 'smtp.gmail.com'){
+
+        console.log("  warning: cannot connect to Gmail, emails may not send successfully")
+      } else {
+        console.log(error)
+      }
     } else {
+      connectedToMailServer = true
       console.log('   mailer service started')
     }
   })
 }
 
 exports.sendMail = function(text, subject, recipients){
-  return transporter.sendMail({
-    from: '"Cohort" <cohortrocks@gmail.com>',
-    to: recipients,
-    subject: subject,
-    text: text
-  })
+  if(!connectedToMailServer){
+    return new Error("Mailer service is not available")
+  } else {
+    return transporter.sendMail({
+      from: '"Cohort" <cohortrocks@gmail.com>',
+      to: recipients,
+      subject: subject,
+      text: text
+    })
+  }
 }
